42a8f0de0bbfd195a5891f2ea30c4f43cd9ec7d7
AlineTheytaz <aline.theytaz@sbw-media.ch>
2024-02-16 11:25:37 +0100

    spiil isch do

--- main.js
+++ main.js
@@ -12,11 +12,15 @@ function jump(){
 }
 
 let isAlive = setInterval(function (){
-    
+    //get current aline Y position
     let alineTop = parseInt( window.getComputedStyle(aline).getPropertyValue("top"));
-    
+    //get current linus X position
     let linusLeft =parseInt(window.getComputedStyle(linus).getPropertyValue("left"));
-    console.log(linusLeft)
+    //detect collision
+    if(linusLeft <50 && linusLeft > 0 && alineTop >=140){
+     //collision
+     alert("du bisch gfresse worde vom linus =(")
+    }
 
 }, 10);
 
