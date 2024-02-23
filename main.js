713fa998b267e219e4d18f63d7349904a7c29d2d
AlineTheytaz <aline.theytaz@sbw-media.ch>
2024-02-23 11:02:40 +0100

    counter jeje

--- main.js
+++ main.js
@@ -20,14 +20,38 @@ let isAlive = setInterval(function (){
     if(linusLeft <50 && linusLeft > 0 && alineTop >=140){
      //collision
      alert("du bisch gfresse worde vom linus =(")
+     spaceBarCounter = 0;
+     updateCounter();
     }
 
 }, 10);
+
+var spaceBarCounter = 0; // Variable zur Verfolgung der Anzahl von Leertastendrücken
+
+// Funktion zum Anzeigen von Nachrichten in der Konsole
+function logToConsole(message) {
+    var consoleDiv = document.getElementById('console');
+    consoleDiv.innerHTML += message + '<br>';
+    consoleDiv.scrollTop = consoleDiv.scrollHeight;
+}
+
+// Funktion zum Aktualisieren des Zählers und Anzeigen im HTML
+function updateCounter() {
+    var counterDiv = document.getElementById('counter');
+    counterDiv.textContent = 'Leertaste wurde ' + spaceBarCounter + ' Mal gedrückt.';
+}
+
 
 document.addEventListener("keydown", function(event){
     if(event.key === " "){
         console.log('Leertaste wurde grdrückt'); 
         jump();
+        spaceBarCounter++;
+        updateCounter();
     }
 });
+
+document.addEventListener("DOMContentLoaded", function(){
+    updateCounter();
+})
 
