98dd1703e3535e01f67b672e6ba5cc6642ccd4a5
linusmaria <linus.hausammann@sbw-media.ch>
2024-03-22 11:04:28 +0100

    halt stop

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const aline = document.getElementById("aline");
 const linus = document.getElementById("linus");
 const linus2 = document.getElementById("linus2");
 const linusvogel = document.getElementById("linusvogel");
+function redirect() {
+    window.location.href = 'gameover.html'
+}
 
 // Definition von loopwindow als benutzerdefiniertes Objekt
 const loopwindow = {
@@ -71,7 +74,80 @@ function startAnimation() {
     // Fügen Sie hier weitere Aktionen nach dem Klick auf "Start" hinzu
 
 
-    //The proper game
+    // VERSION 1
+    let isAlive = setInterval(function () {
+        //get current aline Y position
+        let alineTop = parseInt(window.getComputedStyle(aline).getPropertyValue("top"));
+        //get current linus X position
+        let linusLeft = parseInt(window.getComputedStyle(linus).getPropertyValue("left"));
+        //detect collision
+        if (linusLeft < 50 && linusLeft > 0 && alineTop >= 140) {
+            //collision            
+            redirect();
+        }
+
+    }, 10);
+
+    function jump() {
+        if (aline.classList != "jump") {
+            aline.classList.add("jump");
+
+            setTimeout(function () {
+                aline.classList.remove("jump");
+            }, 300);
+        }
+    }
+
+    document.addEventListener("keydown", function (event) {
+        if (event.key === " ") {
+            console.log('Leertaste wurde grdrückt');
+            jump();
+            spaceBarCounter++;
+            updateCounter();
+        }
+
+        if (event.key === "ArrowDown") { //Pfeiltaste wird gedrückt, Aline geht nach unten
+            duck();
+
+        }
+    });
+
+    function duck() {
+        if (!isDucking) {
+            aline.style.height = '50px';
+            linus.style.bottom = '100px';
+            linus2.style.bottom = '75px';
+            linusvorgel.style.bottom = '-30px';
+            isDucking = true;
+        }
+    }
+
+    function standUp() {
+        aline.style.height = '70px';
+        linus.style.top = '100px';
+        linus2.style.top = '75px';
+        linusvorgel.style.top = '-30px';
+        isDucking = false;
+    }
+
+    // Funktion zum Zurücksetzen des Spiels
+    function resetGame() {
+        // Setze die Position von aline und linus zurück
+        aline.style.top = "150px";
+        linus.style.left = "460px"; // Setze linus außerhalb des Bildschirms
+        // Setze die Höhe von aline zurück
+        standUp(); // Stelle sicher, dass aline steht
+        // Setze den Zähler zurück
+        spaceBarCounter = 0;
+        updateCounter(); 
+
+        // Lade die Seite neu
+        location.reload();
+    }
+
+    
+
+    /** VERSION 2
     loopwindow.requestAnimationFrame(gameLoop);
     function gameLoop() {
         draw();
@@ -158,82 +234,27 @@ function startAnimation() {
             });
         }
 
-        function jump() {
-            if (aline.classList != "jump") {
-                aline.classList.add("jump");
-
-                setTimeout(function () {
-                    aline.classList.remove("jump");
-                }, 300);
-            }
-        }
-
-        document.addEventListener("keydown", function (event) {
-            if (event.key === " ") {
-                console.log('Leertaste wurde grdrückt');
-                jump();
-                spaceBarCounter++;
-                updateCounter();
-            }
-
-            if (event.key === "ArrowDown") { //Pfeiltaste wird gedrückt, Aline geht nach unten
-                duck();
-
-            }
-        });
-
-        function duck() {
-            if (!isDucking) {
-                aline.style.height = '50px';
-                linus.style.bottom = '100px';
-                linus2.style.bottom = '75px';
-                linusvorgel.style.bottom = '-30px';
-                isDucking = true;
-            }
-        }
-
-        function standUp() {
-            aline.style.height = '70px';
-            linus.style.top = '100px';
-            linus2.style.top = '75px';
-            linusvorgel.style.top = '-30px';
-            isDucking = false;
-        }
+        
 
         window.requestAnimationFrame(gameLoop);
-    }
+    }*/
 
 
-    // Funktion zum Zurücksetzen des Spiels
-    function resetGame() {
-        /**  Setze die Position von aline und linus zurück
-        aline.style.top = "150px";
-        linus.style.left = "460px"; // Setze linus außerhalb des Bildschirms
-        // Setze die Höhe von aline zurück
-        standUp(); // Stelle sicher, dass aline steht
-        // Setze den Zähler zurück
-        spaceBarCounter = 0;
-        updateCounter(); */
-
-        // Lade die Seite neu
-        location.reload();
-    }
-
-
-    /** let isAlive = setInterval(function () {
-        //get current aline Y position
-        let alineTop = parseInt(window.getComputedStyle(aline).getPropertyValue("top"));
-        //get current linus X position
-        let linusLeft = parseInt(window.getComputedStyle(linus).getPropertyValue("left"));
-        //detect collision
-        if (linusLeft < 50 && linusLeft > 0 && alineTop >= 140) {
-            //collision
-            alert("du bisch gfresse worde vom linus =(")
-            resetGame();
+    /** 
+        function resetGame() {
+            Setze die Position von aline und linus zurück
+            aline.style.top = "150px";
+            linus.style.left = "460px"; // Setze linus außerhalb des Bildschirms
+            // Setze die Höhe von aline zurück
+            standUp(); // Stelle sicher, dass aline steht
+            // Setze den Zähler zurück 
+            spaceBarCounter = 0;
+            updateCounter(); 
+    
+            // Lade die Seite neu
+            location.reload();
         }
-
-    }, 10);
-    */
+        */
 
 
 
@@ -275,8 +296,6 @@ function startAnimation() {
     });
 
 }
-
-//funktioniert nöd danke
 
 // Funktion zum Laden des Highscores aus dem localStorage
 function loadHighscore() {
